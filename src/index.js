b65f19324fce8f420e9c2d73d801631a528282d6
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-03 01:28:25 +0700

    Use HTML double quote

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import Counter from './components/Counter'
 
 ReactDOM.render(
-  <Counter label={'I\'m a counter'} />,
+  <Counter label="I'm a counter" />,
   document.getElementById('react-root')
 )
